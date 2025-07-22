ab7762901c18a57648f119cf02f91afdc639cbfd
Leo Leplat <60394504+Rylern@users.noreply.github.com>
2025-07-22 16:32:03 +0100

    Add number of threads and pyramidalize parameters

--- src/main/java/qupath/ext/stitching/gui/StitchingAction.java
+++ src/main/java/qupath/ext/stitching/gui/StitchingAction.java
@@ -9,6 +9,8 @@ import qupath.fx.dialogs.Dialogs;
 import qupath.fx.dialogs.FileChoosers;
 import qupath.lib.common.ThreadTools;
 import qupath.lib.gui.QuPathGUI;
+import qupath.lib.gui.tools.GuiTools;
+import qupath.lib.plugins.parameters.ParameterList;
 
 import java.io.File;
 import java.io.IOException;
@@ -41,8 +43,14 @@ class StitchingAction implements Runnable {
     public void run() {
         logger.debug("Stitching action called for {}", quPath);
 
+        ParameterList parameters = createParameterList();
+        if (!GuiTools.showParameterDialog(resources.getString("StitchingAction.title"), parameters)) {
+            return;
+        }
+        logger.debug("Got parameters {} for stitching", parameters);
+
         List<File> inputFiles = FileChoosers.promptForMultipleFiles(
-                resources.getString("stitcherAction.chooseInputImages"),
+                resources.getString("StitchingAction.chooseInputImages"),
                 FileChoosers.createExtensionFilter("TIFF files", ".tif", ".tiff")
         );
         if (inputFiles == null || inputFiles.isEmpty()) {
@@ -51,7 +59,7 @@ class StitchingAction implements Runnable {
         logger.debug("Got files {} to stitch", inputFiles);
 
         File outputFile = FileChoosers.promptToSaveFile(
-                resources.getString("stitcherAction.chooseOutputPath"),
+                resources.getString("StitchingAction.chooseOutputPath"),
                 new File(""),
                 FileChoosers.createExtensionFilter("OME-Zarr", ".ome.zarr")
         );
@@ -64,8 +72,8 @@ class StitchingAction implements Runnable {
             logger.debug("A file already exists at {}. Asking user if possible to delete it", outputFile);
 
             if (!Dialogs.showConfirmDialog(
-                    resources.getString("stitcherAction.warning"),
-                    resources.getString("stitcherAction.fileAlreadyExists")
+                    resources.getString("StitchingAction.warning"),
+                    resources.getString("StitchingAction.fileAlreadyExists")
             )) {
                 return;
             }
@@ -76,28 +84,47 @@ class StitchingAction implements Runnable {
                 logger.error("Cannot delete {} file", outputFile, e);
 
                 Dialogs.showErrorMessage(
-                        resources.getString("stitcherAction.stitchingFailed"),
-                        resources.getString("stitcherAction.fileAlreadyExistsAndCannotBeDeleted")
+                        resources.getString("StitchingAction.stitchingFailed"),
+                        resources.getString("StitchingAction.fileAlreadyExistsAndCannotBeDeleted")
                 );
             }
         }
 
-        stitchImages(inputFiles.stream().map(File::getPath).toList(), outputFile.getPath());
+        stitchImages(inputFiles.stream().map(File::getPath).toList(), outputFile.getPath(), parameters);
     }
 
     /**
      * @return a localized string describing this action
      */
     public static String getTitle() {
-        return resources.getString("stitcherAction.title");
+        return resources.getString("StitchingAction.title");
     }
 
-    private void stitchImages(List<String> inputImages, String outputImage) {
+    private static ParameterList createParameterList() {
+        return new ParameterList()
+                .addIntParameter(
+                        "numberOfThreads",
+                        resources.getString("StitchingAction.numberOfThreads"),
+                        Runtime.getRuntime().availableProcessors(),
+                        "",
+                        1,
+                        Runtime.getRuntime().availableProcessors(),
+                        resources.getString("StitchingAction.numberOfThreadsDescription")
+                )
+                .addBooleanParameter(
+                        "pyramidalize",
+                        resources.getString("StitchingAction.pyramidalize"),
+                        true,
+                        resources.getString("StitchingAction.pyramidalizeDescription")
+                );
+    }
+
+    private void stitchImages(List<String> inputImages, String outputImage, ParameterList parameters) {
         ExecutorService executor = Executors.newSingleThreadExecutor(ThreadTools.createThreadFactory("stitching-action-", false));
         ProgressWindow progressWindow;
         try {
             progressWindow = new ProgressWindow(
-                    resources.getString("stitcherAction.stitchingImages"),
+                    resources.getString("StitchingAction.stitchingImages"),
                     executor::shutdownNow
             );
             progressWindow.initOwner(quPath.getStage());
@@ -110,12 +137,14 @@ class StitchingAction implements Runnable {
 
         executor.execute(() -> {
             try {
-                Platform.runLater(() -> progressWindow.setStatus(resources.getString("stitcherAction.parsingInputImages")));
+                Platform.runLater(() -> progressWindow.setStatus(resources.getString("StitchingAction.parsingInputImages")));
                 ImageStitcher imageStitcher = new ImageStitcher.Builder(inputImages)
+                        .setNumberOfThreads(parameters.getIntParameterValue("numberOfThreads"))
+                        .pyramidalize(parameters.getBooleanParameterValue("pyramidalize"))
                         .setOnProgress(progress -> Platform.runLater(() -> progressWindow.setProgress(progress / 2)))
                         .build();
 
-                Platform.runLater(() -> progressWindow.setStatus(resources.getString("stitcherAction.writingOutputImage")));
+                Platform.runLater(() -> progressWindow.setStatus(resources.getString("StitchingAction.writingOutputImage")));
                 imageStitcher.writeToZarrFile(
                         outputImage,
                         progress -> Platform.runLater(() -> progressWindow.setProgress(0.5f + progress/2))
@@ -125,10 +154,10 @@ class StitchingAction implements Runnable {
                     progressWindow.close();
 
                     Dialogs.showInfoNotification(
-                            resources.getString("stitcherAction.stitchingCompleted"),
+                            resources.getString("StitchingAction.stitchingCompleted"),
                             resources.getString(imageStitcher.areSomeInputImagesNotUsed() ?
-                                    "stitcherAction.someImagesCombined" :
-                                    "stitcherAction.providedImagesCombined"
+                                    "StitchingAction.someImagesCombined" :
+                                    "StitchingAction.providedImagesCombined"
                             )
                     );
                 });
@@ -141,9 +170,9 @@ class StitchingAction implements Runnable {
                     logger.error("Error when stitching {} to {}", inputImages, outputImage, e);
 
                     Platform.runLater(() -> Dialogs.showErrorMessage(
-                            resources.getString("stitcherAction.stitchingFailed"),
+                            resources.getString("StitchingAction.stitchingFailed"),
                             MessageFormat.format(
-                                    resources.getString("stitcherAction.errorWhenStitching"),
+                                    resources.getString("StitchingAction.errorWhenStitching"),
                                     e.getLocalizedMessage()
                             )
                     ));

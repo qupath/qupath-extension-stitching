96c21b48bfb831bf9ba60108ae056e8650980835
lleplat <lleplat@ed.ac.uk>
2025-08-19 09:45:28 +0100

    Update sample scripts

--- sample-scripts/stitch_images_to_zarr.groovy
+++ sample-scripts/stitch_images_to_zarr.groovy
@@ -9,9 +9,15 @@ var inputImages = [
         "/path/to/the/input/image2.tiff",
         // other images...
 ]
-var outputImage = "/path/to/the/output/image.ome.zarr"      // the path must ends with ".ome.zarr" and must not already exist
+var outputImage = "/path/to/the/output/image.ome.zarr"          // the path must ends with ".ome.zarr" and must not already exist
+var imageSource = ImageStitcher.ImageSource.VECTRA_3                   // the type of the input images. Can also be ImageStitcher.ImageSource.VECTRA_2
+var numberOfThreads = Runtime.getRuntime().availableProcessors()  // the number of threads to use when reading and writing files
+var pyramidalize = true                                       // whether to create a pyramidal image
 
 new ImageStitcher.Builder(inputImages)
+    .imageSource(imageSource)
+    .numberOfThreads(numberOfThreads)
+    .pyramidalize(pyramidalize)
     .build()
     .writeToZarrFile(outputImage, null)
 
